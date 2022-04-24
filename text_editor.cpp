465447972ede416e1272ebaa3f965e21d0af3d7e
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-04-24 15:40:37 -0300

    finished project

--- text_editor.cpp
+++ text_editor.cpp
@@ -1,6 +1,7 @@
 #include "text_editor.h"
 #include "ui_text_editor.h"
 
+
 Text_Editor::Text_Editor(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Text_Editor)
@@ -15,9 +16,178 @@ Text_Editor::~Text_Editor()
     delete ui;
 }
 
-void Text_Editor::on_action_file_triggered()
+
+
+
+
+void Text_Editor::on_action_new_file_triggered()
 {
+    filenameCurrent = "";
     ui->text_edit->clear();
     ui->text_edit->setFocus();
+}
+
+
+void Text_Editor::on_action_open_triggered()
+{
+    QString nameFile = QFileDialog::getOpenFileName(this,"Open file",QDir::homePath(),allFiles);
+    QFile file(nameFile);
+    filenameCurrent = nameFile; // garantir que ao abrir vai possuir nessa variavael um nome de arquivo,ele sera comparado ao longo da app
+
+    if(!file.open(QFile::ReadOnly | QFile::Text) && file.exists()){//arquivos que tem permissao de leitura r e existem
+
+         QMessageBox::warning(this,"Can`t open file","Probaly this file need permission");
+         return;
+    }
+
+    QTextStream stream(&file);
+    QString text= stream.readAll();
+    ui->text_edit->setText(text);
+    file.close();// e importante fechar  o file para o editor conseguir seguir em frente,enquanto  o file estiver aberto ele nao consegue
+}
+
+
+
+void Text_Editor::on_action_save_with_triggered()
+{
+
+     QString nameFile = QFileDialog::getSaveFileName(this,"Save file",QDir::homePath(),allFiles);
+     QFile file(nameFile);
+     filenameCurrent = nameFile;
+
+     if(filenameCurrent.isEmpty()) return;
+
+     if(!file.open(QFile::WriteOnly | QFile::Text)){//arquivos que nao tem permissao de escrita
+
+         QMessageBox::warning(this,"Can`t save file","Probraly need permission");
+         return;
+     };
+     QTextStream stream(&file);
+     QString text = ui->text_edit->toPlainText();
+     stream << text << "\n";
+
+     file.flush();
+     file.close();// e importante fechar  o file para o editor conseguir seguir em frente,enquanto  o file estiver aberto ele nao consegue
+
+}
+
+
+void Text_Editor::on_action_save_triggered()
+{
+
+
+      //nos estamos comparando o filenameCurrent para realizar a logica,ideal quando clicar em salvar  verifcar se e arquivo novo ou nao
+      //entao preico colocar  variavel filnemaeCurrente vazio em new file,porque e um arquivo novo,assim  vai entrar em action_save_with
+     QFile file(filenameCurrent);
+
+
+
+     if(filenameCurrent.isEmpty()  || !file.exists()) {
+         on_action_save_with_triggered();
+         return;
+
+     }
+
+
+     if(!file.open(QFile::WriteOnly | QFile::Text)){
+         QMessageBox::warning(this,"Error save","Probaly need permission save");
+         return;
+     }
+
+
+     if(file.exists()){//trabalha de forma assincrona por isso preciso colocar file.exists()
+         QTextStream stream(&file);
+         QString text = ui->text_edit->toPlainText(); //estou pegando o arquivo que esta nosso editor e eviando para o computador dele
+         stream << text << "\n";
+
+         file.flush();
+         file.close();
+
+     }
+
+}
+
+void Text_Editor::on_action_copy_triggered()
+{
+    ui->text_edit->copy();
+}
+
+
+void Text_Editor::on_action_paste_triggered()
+{
+    ui->text_edit->paste();
+}
+
+
+void Text_Editor::on_action_cut_triggered()
+{
+    ui->text_edit->cut();
+}
+
+
+void Text_Editor::on_action_redo_triggered()
+{
+    ui->text_edit->redo();
+}
+
+
+void Text_Editor::on_action_undo_triggered()
+{
+    ui->text_edit->undo();
+}
+
+
+void Text_Editor::on_action_color_triggered()
+{
+    QColor color = QColorDialog::getColor(Qt::black,this,"Chosse your color");
+    if(color.isValid()){
+
+        ui->text_edit->setTextColor(color);
+    }
+}
+
+
+void Text_Editor::on_action_font_triggered()
+{
+    bool ok;
+    QFont font = QFontDialog::getFont(&ok,this);
+
+    if(ok) {
+
+        ui->text_edit->setFont(font);
+
+    }
+
+}
+
+
+void Text_Editor::on_action_background_triggered()
+{
+    QColor color = QColorDialog::getColor(Qt::black,this,"Chosse your background");
+    if(color.isValid()){
+
+        ui->text_edit->setTextBackgroundColor(color);
+    }
+}
+
+
+void Text_Editor::on_action_about_triggered()
+{
+    QMessageBox::about(this,"About program","This progam was created terminal root couse");
+}
+
+
+void Text_Editor::on_actionPrint_triggered()
+{
+     QPrinter printer;
+     QPrintDialog printerDialog;
+
+
+     if(printerDialog.exec() == QDialog::Rejected){
+         QMessageBox::critical(this,"Can`t print","Error on pint");
+         return;
+     }
+
+     ui->text_edit->print(&printer);
 }
 
--- text_editor.h
+++ text_editor.h
@@ -2,6 +2,16 @@
 #define TEXT_EDITOR_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QColorDialog>
+#include <QColor>
+#include <QFont>
+#include <QFontDialog>
+#include <QPrintDialog>
+#include <QPrinter>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Text_Editor; }
@@ -14,11 +24,41 @@ class Text_Editor : public QMainWindow
 public:
     Text_Editor(QWidget *parent = nullptr);
     ~Text_Editor();
+    QString filenameCurrent = "";
+    QString allFiles = "File text (*.txt);;Makdown  text(*.md)"; //tipos de arquivos permitidos, File text vai aparecer no canto inferior,importante e a extensao *.txt,*.md
 
 private slots:
 
 
-    void on_action_file_triggered();
+
+
+    void on_action_open_triggered();
+
+    void on_action_new_file_triggered();
+
+    void on_action_save_with_triggered();
+
+    void on_action_save_triggered();
+
+    void on_action_copy_triggered();
+
+    void on_action_paste_triggered();
+
+    void on_action_cut_triggered();
+
+    void on_action_redo_triggered();
+
+    void on_action_undo_triggered();
+
+    void on_action_color_triggered();
+
+    void on_action_font_triggered();
+
+    void on_action_background_triggered();
+
+    void on_action_about_triggered();
+
+    void on_actionPrint_triggered();
 
 private:
     Ui::Text_Editor *ui;
